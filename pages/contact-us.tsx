9eaa6b32a89bc77d08d1b03f82aa13a5aebae648
Gurinder Singh <gurinderrawala@gmail.com>
2022-12-04 15:43:01 -0500

    added contact us

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,5 +1,18 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import { ContactUsComponent } from "../components/contact-us-component";
 
-const ContactUs: NextPage = () =>(<h1>Contact Us</h1>)
+const ContactUs: NextPage = () =>(
+    <>
+        <Head>
+            <title>Toor Welding | Contact Us</title>
+            <meta
+                name="description"
+                content="Toor Welding provides best welding service in GTA and Brampton"
+            />
+        </Head>
+        <ContactUsComponent />
+    </>
+)
 
 export default ContactUs
